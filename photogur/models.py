c344a9f3cf44d54d912648b5629523b6f37dd3db
adam-weiler <adam.weiler@yahoo.ca>
2019-08-06 13:35:16 -0400

    Adding user field to Picture model, making/migrating changes. Showing username on pictures page. Also adding Signup button to base.html.

--- photogur/models.py
+++ photogur/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -5,10 +6,12 @@ class Picture(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'title={self.title}, artist={self.artist}, url={self.url}'
         return f'<img src=\'{self.url}\' alt=\'{self.title}\' />'
+
 
 class Comment(models.Model):
     name = models.CharField(max_length=255)
